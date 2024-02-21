287739274fe35f7170e7e8903cc14723f6268954
trieuphan <trieuphan0211@gmail.com>
2024-02-21 17:33:44 +0700

    add ads.txt

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,9 +17,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <header>
-        <meta name="google-adsense-account" content="ca-pub-5486007501237656" />
-      </header>
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
--- auth.ts
+++ auth.ts
@@ -30,7 +30,6 @@ export const { auth, signIn, signOut } = NextAuth({
           const user = await getUser(email);
           if (!user) return null;
           const passwordsMatch = await bcrypt.compare(password, user.password);
-
           if (passwordsMatch) return user;
         }
         console.log('Invalid credentials');
